57499da7969ad0e83c952b9fd86a41e129f1b81f
Jeff Muñoz <jmunozcastro@protonmail.com>
2021-11-22 18:50:35 -0600

    View about created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'welcome/about'
   get 'welcome/index'
   root 'welcome#index' #página principal
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
